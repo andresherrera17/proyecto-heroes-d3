b352279958dabf649d5693c19171c8948e002133
Yaricsa camacho <ycgomez33@gmail.com>
2022-07-27 20:07:06 -0500

    Crear rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EjemplosPipesComponent } from './ejemplos-pipes/ejemplos-pipes.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'pipes', component: EjemplosPipesComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
