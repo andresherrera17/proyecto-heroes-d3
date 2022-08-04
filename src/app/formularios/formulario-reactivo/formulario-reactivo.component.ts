ccebc36b049e823d2abe9925c8640c4430ad1083
Yaricsa camacho <ycgomez33@gmail.com>
2022-08-03 20:19:21 -0500

    Errores

--- src/app/formularios/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formularios/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
@@ -19,13 +19,53 @@ export class FormularioReactivoComponent implements OnInit {
 
   buildForm(){
     this.form = this.formBuilder.group({
-      nombre : [''],
-      apellido: [''],
-      pais: ['']
+      nombre : ['',Validators.required],
+      apellido: ['',Validators.required],
+      email:['',[Validators.required, Validators.email]],
+      pass:['',[Validators.required, this.validarContrasena]],
+      pais: [''],
+      direccion: this.formBuilder.group({
+        calle: ['',Validators.required],
+        avenida: ['',Validators.required]
+      })
     });
+  }
+
+  validarContrasena(control: AbstractControl){
+    const contrasena:string = control.value;
+    let error = null;
+    if(!contrasena.includes('$')){
+      error = { pesos:'Es requerido el signo pesos' }
+    }
+    if(!parseFloat(contrasena[0])){
+      error = { ...error, number:'Debe empezar con un número' }
+    }
+
+    return error;
   }
 
   guardar(){
     console.log(this.form);
   }
+
+  get usuarioRequired(){
+    if(this.nombre?.errors){
+      return this.nombre?.errors["required"];
+    }else{
+      return false;
+    }
+  }
+
+  getError(controlName:string){
+    let error = null;
+    const control = this.form.get(controlName);
+    if(control?.errors != null){
+      error = control?.errors;
+    }
+
+    return error;
+  }
+
+  get nombre(): AbstractControl | null { return this.form.get('nombre')};
+  get apellido(): AbstractControl | null { return this.form.get('apellido')};
 }
